693bf5c7e33555771808764b34b1c76b8d7d0c38
vicente <118966614+vsbb2123-git@users.noreply.github.com>
2023-12-04 08:46:08 +0100

    Update Ejercicio5.java

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -3,3 +3,62 @@ Modifica el programa anterior para que el hilo principal no espere 4 segundos si
 que se espere a que acaben los dos hilos que crea. El hilo principal ha de mostrar por pantalla
 cuando empieza y cuando acaba.
 */
+
+class HiloEjercicio5 extends Thread {
+	@Override
+	public void run() {
+		try {
+			System.out.println("Voy crear dos hilos y a esperar a que acaben.");
+            HiloEspera1 hilo1 = new HiloEspera1 ();
+            HiloEspera2 hilo2 = new HiloEspera2 ();
+            hilo1.setName("hilo1");
+            hilo2.setName("hilo2");
+            hilo1.start();
+            hilo2.start();
+			hilo2.join();
+		} catch (InterruptedException e) {
+			System.out.println("Hilo creado interrumpido.");
+			return;
+		}
+    
+		System.out.println("Soy el hilo principal que va a acabar.");
+
+	}
+}
+
+class HiloEspera1 extends Thread {
+	@Override
+	public void run() {
+		System.out.println("soy el hilo 1 y voy a esperar 3 segundos.");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			System.out.println("Hilo 1 interrumpido.");
+			return;
+		}
+		System.out.println("Soy el hilo1 que va a acabar.");
+
+	}
+}
+
+class HiloEspera2 extends Thread {
+	@Override
+	public void run() {
+		System.out.println("soy el hilo 2 y voy a esperar 5 segundos.");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			System.out.println("Hilo 2 interrumpido.");
+			return;
+		}
+		System.out.println("Soy el hilo2 que va a acabar.");
+
+	}
+}
+
+public class ejercicio5 {
+	public static void main(String args[]) {
+		HiloEjercicio5 hilo = new HiloEjercicio5();
+		hilo.start();
+	}	
+}
