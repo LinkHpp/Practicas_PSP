f81991c669c28c7dafb5d8c01c33384f92a04d03
Albert Lozano <albertlb08@gmail.com>
2023-12-04 08:42:21 +0100

    Ejercicio 8 terminado

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -6,5 +6,52 @@ van a esperar y que han acabado.
 */
 
 public class Ejercicio8 {
-    
+
+    public static void main(String[] args) {
+        Thread mainThread = Thread.currentThread();
+        System.out.println("Hilo principal: " + mainThread.getName());
+
+        createAndStartThreads(10);
+    }
+
+    private static void createAndStartThreads(int threadsToCreate) {
+        if (threadsToCreate > 0) {
+            Thread newThread = new Thread(() -> {
+                Thread currentThread = Thread.currentThread();
+                System.out.println("Hilo creado: " + currentThread.getName());
+
+                System.out.println("Hilo " + currentThread.getName() + " va a empezar.");
+
+                //Crear hilo hijo recursivamente
+                createAndStartThreads(threadsToCreate - 1);
+
+                //Esperar al hilo hijo
+                try {
+                    System.out.println("Hilo " + currentThread.getName() + " va a esperar.");
+                    Thread.sleep(1000); // Simula algún trabajo en el hilo actual
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("Hilo " + currentThread.getName() + " ha acabado.");
+            });
+
+            newThread.start();
+
+            //Esperar a que el hilo hijo termine antes de continuar
+            try {
+                newThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            //Último hilo creado espera 5 segundos
+            try {
+                System.out.println("Último hilo creado. Esperando 5 segundos...");
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
